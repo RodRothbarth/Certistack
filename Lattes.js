805fe292a32868190c2fb0f1d16f8f93da995fab
Rodrothbarth <rodrothbarth@gmail.com>
2020-10-05 21:23:10 -0300

    teste cadastros realizados

--- Lattes.js
+++ Lattes.js
@@ -1,12 +1,12 @@
-// $(document).ready(function(){
-//     $('.CPF').mask('000.000.000-00');
-//     $('.numCelular').mask('(99) 99999-9999');
-//   });
+$(document).ready(function(){
+    $('.CPF').mask('000.000.000-00');
+    $('.numCelular').mask('(99) 99999-9999');
+  });
 
 let dataBase = [];
 let dataCerti = []; 
 
-function User (name, cpf, email, dataNasc, interrest, pswd, university, lattes){ //função construtora para o cadastro.
+function User (name, cpf, email, cellphone, dataNasc, pswd, lattes, interrest, university ){ //função construtora para o cadastro.
     this.nome = name;
     this.cpf = cpf;
     this.email = email;
@@ -15,20 +15,15 @@ function User (name, cpf, email, dataNasc, interrest, pswd, university, lattes){
     this.pswd = pswd;
     this.university = university;
     this.site = lattes;
+    this.celular = cellphone;
 };
 
 function Cadastrar(){ //sistema para cadastrar um novo usuario.
-    // ADICIONAR TELEFONE CELULAR
-    let name = document.getElementById('name');
-    let cpf = document.getElementById('cpf');
-    let email = document.getElementById('email');
-    let dataNasc = document.getElementById('dataNasc');
-    let interrest = document.getElementById('interrest');
-    let pswd = document.getElementById('pswd');
-    let college = document.getElementById('college');
-    let lattes = document.getElementById('lattes');
-    let user = new User(name.value, cpf.value, email.value, dataNasc.value, interrest.value, pswd.value, college.value, lattes.value);
-    dataBase.push(user);
+    let user = Array.from(document.getElementsByName("cadastro")).map(function(element){return element.value;});
+    dataBase.push(new User(user[0], user[1], user[2], user[3], user[4], user[5], user[7], user[8], user[9]));
+    localStorage.setItem("user", JSON.stringify(dataBase));
+    alert("Cadastro Realizado com Sucesso!");
+
 }
 
 function validation(){
@@ -41,7 +36,7 @@ function validation(){
         }else if (dataBase[i].cpf == usern.value || dataBase[i].email == usern.value){
             if(dataBase[i].pswd == pssw.value){
                 console.log("Sucesso");
-                localStorage.setItem('user', JSON.stringify(dataBase[i]));
+                localStorage.setItem('online', JSON.stringify(dataBase[i]));
                 location.href="perfil.html"; // site à ser feita de perfil do usuario validado, ou window.open("home.html") para abrir em uma nova aba
             }else{
                 alert("Senha Incorreta");
@@ -54,8 +49,8 @@ function validation(){
 }
 
 function Exit(){ // botão para sair do perfil validado para troca de perfil ou saida "segura" do sistema. 
-    localStorage.remove("user");
-    location.href="home.html"; //ou window.open("home.html") para abrir em uma nova aba
+    localStorage.remove("online");
+    location.href="index.html"; //ou window.open("home.html") para abrir em uma nova aba
 }
 
 function AddHour(event, beginEvent,  endEvent, hour,type ){
@@ -68,11 +63,9 @@ function AddHour(event, beginEvent,  endEvent, hour,type ){
 
 function Add(){
     let certificado = Array.from(document.getElementsByName("addCerti")).map(function(element){
-        return element.value;});
-        alert(certificado); 
-    let add = new AddHour (certificado[0], certificado[1], certificado[2], certificado[3], certificado[4])
-    dataCerti.push(add)
+        return element.value;});  
+    dataCerti.push(new AddHour (certificado[0], certificado[1], certificado[2], certificado[3], certificado[4]))
     localStorage.setItem("certificate", JSON.stringify(dataCerti))
+    alert("Certificado Adicionado com Sucesso!")
     // document.getElementById('evento').innerHTML = add; //aqui tem que colocar uma construção de tabela para adicionar as informações de forma a visualizar 
 }
-isadbvpsihdfvb
