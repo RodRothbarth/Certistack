6bd4bc43e0a7a3dfa94f99da24c93fefbca29e8e
Rodrothbarth <rodrothbarth@gmail.com>
2020-10-08 19:47:19 -0300

    verificacao email e senha

--- Lattes.js
+++ Lattes.js
@@ -5,6 +5,9 @@
 
 let dataBase = [];
 let dataCerti = []; 
+let user = Array.from(document.getElementsByName("cadastro")).map(function(element){return element.value;});
+let usern = document.getElementById("cpf");
+let pssw = document.getElementById("senha");
 
 function User (name, cpf, email, cellphone, dataNasc, pswd, lattes, interrest, university ){ //função construtora para o cadastro.
     this.nome = name;
@@ -19,16 +22,51 @@ function User (name, cpf, email, cellphone, dataNasc, pswd, lattes, interrest, u
 };
 
 function Cadastrar(){ //sistema para cadastrar um novo usuario.
-    let user = Array.from(document.getElementsByName("cadastro")).map(function(element){return element.value;});
-    dataBase.push(new User(user[0], user[1], user[2], user[3], user[4], user[5], user[7], user[8], user[9]));
-    localStorage.setItem("user", JSON.stringify(dataBase));
-    alert("Cadastro Realizado com Sucesso!");
+    if (ValidEmail()==false || ValidPass()==false || !user){ // confirma se os campos foram preenchidos com a mesma senha  VERIFICAR COMO ATESTAR TREU OU FALSE NO RETURN
+        alert("TODOS OS CAMPOS DEVEM SER PREENCHIDOS CORRETAMENTE!")
+    }else{
+        if (localStorage.getItem("user") === null ){
+            dataBase.push(new User(user[0], user[1], user[2], user[3], user[4], user[5], user[7], user[8], user[9]));
+            localStorage.setItem("user", JSON.stringify(dataBase));
+            alert("Cadastro Realizado com Sucesso!");
+        }else{
+            dataBase = JSON.parse(localStorage.getItem("user"))
+            dataBase.push(new User(user[0], user[1], user[2], user[3], user[4], user[5], user[7], user[8], user[9]));
+            localStorage.setItem("user", JSON.stringify(dataBase));
+            alert("Cadastro Realizado com Sucesso!");
+        }
+    } 
 }
 
-function Validation(){
-//validação de login e senha para entrar na area de perfil.
-    let usern = document.getElementById("cpf");
-    let pssw = document.getElementById("senha");
+function ValidEmail(field){
+    let address = field.value.substring(0, field.value.indexOf("@"));
+    let domain = field.value.substring(field.value.indexOf("@")+1, field.value.length);
+
+    if ((address.length >=1)&&
+        (domain.length>=3)&&
+        (adress.search("@")==-1)&&
+        (domain.search("@")==-1)&&
+        (address.search(" ")==-1)&&
+        (domain.search(" ")==-1)&&
+        (domain.search(".")!=-1)&&
+        (domain.indexOf(".")>=1)&&
+        (domain.lastIndexOf(".")< domain.length - 1)){
+            document.getElementById("msgEmail").innerHTML = "E-mail Válido"
+            return true
+    }else{
+            document.getElementById("msgEmail").innerHTML = "E-mail Inválido"
+            return false
+        }
+}
+
+function ValidPass(){
+    if (user[5] === user[6]){
+        return true
+    }
+}
+
+function Validation(){ //validação de login e senha para entrar na area de perfil.
+    
     for (i = 0; i <= dataBase.length; i++){
         if(i == dataBase.length){
             alert("Login Incorreto");
@@ -61,48 +99,41 @@ function AddHour(event, beginEvent, endEvent, hour, type){
 }
 
 function Add(){
-
     let certificado = Array.from(document.getElementsByName("addCerti")).map(function(element){
         return element.value;});
-        let day1 = new Date(document.getElementById("day1").value.split('-').join('/')) 
-        let dataFormatada = ("0" + day1.getDate()).substr(-2) + "/" 
-        + ("0" + (day1.getMonth() + 1)).substr(-2) + "/" + day1.getFullYear();
-        let day2 = new Date(document.getElementById("day2").value.split('-').join('/'))
-        let dayFormat = ("0" + day2.getDate()).substr(-2) + "/" 
-        + ("0" + (day2.getMonth() + 1)).substr(-2) + "/" + day2.getFullYear(); //uso para formatar a data em pt-br
-
-    if (certificado[0] && certificado[1] && dayFormat && dataFormatada){
+    let day1 = new Date(document.getElementById("day1").value.split('-').join('/'));
+    let day2 = new Date(document.getElementById("day2").value.split('-').join('/'));
+    let dataFormatada = ("0" + day1.getDate()).substr(-2) + "/" + ("0" + (day1.getMonth() + 1)).substr(-2) + "/" + day1.getFullYear();
+    let dayFormat = ("0" + day2.getDate()).substr(-2) + "/" + ("0" + (day2.getMonth() + 1)).substr(-2) + "/" + day2.getFullYear(); //uso para formatar a data em pt-br
     
+    if (certificado[0], certificado[1] && dayFormat && dataFormatada){
         if (localStorage.getItem("certificate") === null ){
-            dataCerti.push(new AddHour (certificado[0], dataFormatada, dayFormat, certificado[1], certificado[2]))
-                localStorage.setItem("certificate", JSON.stringify(dataCerti))
-                alert("Certificado adicionado com Sucesso!")
+            dataCerti.push(new AddHour (certificado[0], dataFormatada, dayFormat, certificado[1], certificado[2]));
+                localStorage.setItem("certificate", JSON.stringify(dataCerti));
+                alert("Certificado adicionado com Sucesso!");
         } else { 
-            
-            dataCerti = JSON.parse(localStorage.getItem("certificate"))  // for? //[teste 1]
-            dataCerti.push(new AddHour (certificado[0], dataFormatada, dayFormat, certificado[1], certificado[2]))
-            localStorage.setItem("certificate", JSON.stringify(dataCerti))
-            alert("Certificado adicionado com Sucesso!")
+            dataCerti = JSON.parse(localStorage.getItem("certificate"));  
+            dataCerti.push(new AddHour (certificado[0], dataFormatada, dayFormat, certificado[1], certificado[2]));
+            localStorage.setItem("certificate", JSON.stringify(dataCerti));
+            alert("Certificado adicionado com Sucesso!");
         }
     }else{  
-            alert("Todos os campos devem ser preenchidos.")
+        alert(certificado, dayFormat, dataFormatada)
+        alert("Todos os campos devem ser preenchidos.");
         }  
             
 }
 
 function RemoveCert(){
-    localStorage.removeItem("certificate")
-    let confirm = prompt("Digite o nome do evento para confirmar:")
-    
-    for (let i=0; i > dataCerti.length; i++){
-        if(dataCerti[i].evento === confirm){
-            localStorage.removeItem("certificate")
-        }
-    }
-}
-   
-function Teste(){
-    localStorage.removeItem("certificate")
-}
-// if(localStorage.certificate === undefined ){alert("aqui!")
-// }else {localStorage.remove("certificate")}
+   // let confirm = prompt("Digite o nome do evento para confirmar:");
+    dataCerti = JSON.parse(localStorage.getItem("certificate"));
+    let test = Object.create({}, { getFoo: { value: function() { return this.evento; } } });
+    console.log(test);
+    // for (let i=0; i < dataCerti.length; i++){
+        
+    //     if(Object.keys(dataCerti[i].evento) === confirm){
+    //         dataCerti.splice(dataCerti[i],1);
+    //         alert(dataCerti);
+    //     };
+    // };
+};
