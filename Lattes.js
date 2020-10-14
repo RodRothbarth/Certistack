334aff071eb5eb577339128c027c4930c8c5a420
@rafamatosagronomo <rafael_de-matos@estudante.sc.senai.br>
2020-10-14 17:33:19 -0300

    atualizacaoCertificateTable

--- Lattes.js
+++ Lattes.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){
     $('.CPF').mask('000.000.000-00');
     $('.numCelular').mask('(99) 99999-9999');
+    $(".dataCertInicio").mask("00/00/0000");
+    $(".dataCertFim").mask("00/00/0000");
   });
 
 let dataBase = [];
+let dataCerti = []; 
 
 function User (name, cpf, email, dataNasc, interrest, pswd, university, lattes){ //função construtora para o cadastro.
     this.nome = name;
@@ -17,17 +20,10 @@ function User (name, cpf, email, dataNasc, interrest, pswd, university, lattes){
 };
 
 function Cadastrar(){ //sistema para cadastrar um novo usuario.
-    // ADICIONAR TELEFONE CELULAR
-    let name = document.getElementById('name');
-    let cpf = document.getElementById('cpf');
-    let email = document.getElementById('email');
-    let dataNasc = document.getElementById('dataNasc');
-    let interrest = document.getElementById('interrest');
-    let pswd = document.getElementById('pswd');
-    let college = document.getElementById('college');
-    let lattes = document.getElementById('lattes');
-    let user = new User(name.value, cpf.value, email.value, dataNasc.value, interrest.value, pswd.value, college.value, lattes.value);
-    dataBase.push(user);
+    let user = Array.from(document.getElementsByName("cadastro")).map(function(element){return element.value;});
+    dataBase.push(new User(user[0], user[1], user[2], user[3], user[4], user[5], user[7], user[8], user[9]));
+    localStorage.setItem("user", JSON.stringify(dataBase));
+    alert("Cadastro Realizado com Sucesso!");
 }
 
 function validation(){
@@ -57,67 +53,101 @@ function Exit(){ // botão para sair do perfil validado para troca de perfil ou 
     location.href="home.html"; //ou window.open("home.html") para abrir em uma nova aba
 }
 
-function AddHour(event, hour, type, date){
-    this.evento = event;
-    this.data = date;
-    this.horas = hour;
-    this.tipo = type; //tem 3 tipos: Oficinas, cursos extracurriculares e eventos academicos.
+// function AddHour(event, hour, type, date){
+//     this.evento = event;
+//     this.data = date;
+//     this.horas = hour;
+//     this.tipo = type; //tem 3 tipos: Oficinas, cursos extracurriculares e eventos academicos.
+// }
+
+function Add(){ //adicopnar certificados (array para os certificados)
+    let certificado;
+    Array.from(document.getElementsByName("addCerti")).forEach(function(element){
+    certificado[element.keys] = element.value;});
+    // let certificado = Array.from(document.getElementsByName("addCerti")).map(function(element){
+    //     return element.value;});
+    console.log(certificado)
+    if (certificado){
+        if (localStorage.getItem("certificate") === null ){
+            dataCerti.push(certificado);
+                localStorage.setItem("certificate", JSON.stringify(dataCerti));
+                alert("Certificado adicionado com Sucesso!");
+        } else { 
+            dataCerti = JSON.parse(localStorage.getItem("certificate"));  
+            dataCerti.push(certificado);
+                localStorage.setItem("certificate", JSON.stringify(dataCerti));
+                alert("Certificado adicionado com Sucesso!");
+        }
+    }else{  
+        alert("Todos os campos devem ser preenchidos.");
+        }  
+            
+}
+function RemoveCert(){
+    localStorage.removeItem("certificate")
+    let confirm = prompt("Digite o nome do evento para confirmar:")
+    
+    for (let i=0; i > dataCerti.length; i++){
+        if(dataCerti[i].evento === confirm){
+            localStorage.removeItem("certificate")
+        }
+    }
+}
+   
+function Teste(){
+    localStorage.removeItem("certificate")
 }
 
-function Add(){
-    let evento = document.getElementById('titulo');
-    let data = document.getElementById('data');
-    let horas = document.getElementById('horas');
-    let tipo = document.getElementById("tipo")
+let linhasTabela = getDataCertificate()
 
-    let add = new Addhour (evento.value, horas.value, tipo.value, data.value)
-    document.getElementById('evento').innerHTML = add; //aqui tem que colocar uma construção de tabela para adicionar as informações de forma a visualizar 
+function getDataCertificate() {
+    return JSON.parse(localStorage.getItem("certificate"));
 }
 
 function criaTag(elemento) {
     return document.createElement(elemento)
-    }
+}
+        
+    let titulo = document.querySelector("h1");
     
-    let titulo = document.querySelector("section h1");
-    titulo.textContent = "A DOS ALUNOS";
-    
-    let tabela = document.getElementById("tabela");
-    
+   let tabela = document.getElementById("tabela");
+        
     let thead = criaTag("thead");
     let tbody = criaTag("tbody");
     let tfoot = criaTag("tfoot");
-    
-    let indicesTabela = ["Nome do Aluno", "Nota 1", "Nota 2", "Nota 3", "Nota 4", "Media", "Status"];
+        
+    let indicesTabela = ["Evento", "Data Inicial", "Data Final", "Horas", "Tipo"];
+       
     let linhaHead = criaTag("tr");
-    
+        
     function criaCelula(tag, text) {
         tag = criaTag(tag);
         tag.textContent = text;
         return tag;
-    }
-    
-    for(j = 0; j < indicesTabela.length; j++) {
-        let th = criaCelula("th", indicesTabela [j]);
-        linhaHead.appendChild(th);
-    }
-    thead.appendChild(linhaHead);
-    
-    
-    for(j = 0; j < linhasTabela.length; j++) {
-        let linhaBody = criaTag("tr");
-    
-        for(i = 0; i < linhasTabela[j].length; i++) {
-            cel = criaCelula("td", linhasTabela[j][i]);
-            linhaBody.appendChild(cel); 
         }
+        
+        for(j = 0; j < indicesTabela.length; j++) {
+            let th = criaCelula("th", indicesTabela [j]);
+            linhaHead.appendChild(th);
+        }
+        thead.appendChild(linhaHead);
+        
+        
+        for(j = 0; j < linhasTabela.length; j++) {
+            let linhaBody = criaTag("tr");
+        
+            for(i = 0; i < linhasTabela[j].length; i++) {
+                cel = criaCelula("td", linhasTabela[j][i]);
+                linhaBody.appendChild(cel); 
+            }
         tbody.appendChild(linhaBody);
-    }
+        }
     let linhaFoot = criaTag("tr");
-    let celulaFoot = criaCelula("td","Colégio Floripa");
-    celulaFoot.setAttribute("colspan",7);
+    let celulaFoot = criaCelula("td","Certistack");
+    celulaFoot.setAttribute("colspan",5);
     linhaFoot.appendChild(celulaFoot);
     tfoot.appendChild(linhaFoot);
-    
+        
     tabela.appendChild(thead);
     tabela.appendChild(tbody);
     tabela.appendChild(tfoot);
