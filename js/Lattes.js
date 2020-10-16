d431f1e0eb1464368d40eefc0fce891ab6cbe840
Rodrothbarth <rodrothbarth@gmail.com>
2020-10-16 11:06:56 -0300

    organizacao do sistema

--- js/Lattes.js
+++ js/Lattes.js
@@ -1,78 +1,44 @@
-// comimit pull e push
-
 $(document).ready(function(){
     $('.CPF').mask('000.000.000-00');
-    $('.numCelular').mask('(00) 00000-0000');
-    $(".dataNasc").mask("00/00/0000");
+    $('.numCelular').mask('(99) 99999-9999');
     $(".dataCertInicio").mask("00/00/0000");
     $(".dataCertFim").mask("00/00/0000");
   });
 
 let dataBase = [];
 let dataCerti = []; 
+let user = Array.from(document.getElementsByName("cadastro")).map(function(element){return element.value;});
+let usern = document.getElementById("cpf");
+let pssw = document.getElementById("senha");
 
-function User (name, email, pswd, account, documents, cellphone, dataNasc, university, area, lattes, certificates){ //função construtora para o cadastro.
-    this.conta = account;
-    this.area = area;
-    this.celular = cellphone;
-    this.certificados = certificates;
-    this.dataNasc = dataNasc;
+function User (name, documents, email, cellphone, dataNasc, pswd, lattes, interrest, university, account, certificates ){ //função construtora para o cadastro.
+    this.nome = name;
     this.documento = documents;
     this.email = email;
-    this.site = lattes;
-    this.nome = name;
+    this.dataNasc = dataNasc;
+    this.interesse = interrest;
     this.pswd = pswd;
     this.university = university;
+    this.site = lattes;
+    this.celular = cellphone;
+    this.conta = account;
+    this.certificados = certificates;
 };
 
 function Cadastrar(){ //sistema para cadastrar um novo usuario.
-let user = Array.from(document.getElementsByName("cadastro")).map(function(element){return element.value;});
-    
-    if (localStorage.getItem("user") === null ){
-        dataBase.push(new User(user[0], user[1], user[2],user[4], user[5], user[6], user[7], user[8], user[9]));
-        localStorage.setItem("user", JSON.stringify(dataBase));
-        alert("Cadastro Realizado com Sucesso!");
-        $(".login").show();
-        $(".cadastro").hide();
+    if (!ValidPass() || !user){ // confirma se os campos foram preenchidos com a mesma senha  VERIFICAR COMO ATESTAR TREU OU FALSE NO RETURN
+        alert("TODOS OS CAMPOS DEVEM SER PREENCHIDOS CORRETAMENTE!")
     }else{
-        dataBase = JSON.parse(localStorage.getItem("user"))
-        dataBase.push(new User(user[0], user[1], user[2],user[4], user[5], user[6], user[7], user[8], user[9]));
-        localStorage.setItem("user", JSON.stringify(dataBase));
-        alert("Cadastro Realizado com Sucesso!");
-        $(".login").show();
-        $(".cadastro").hide();
-    }
-}
-
-function TestaCPF(strCPF) {
-    var Soma = 0;
-    var Resto;
-    
-  if (strCPF == "00000000000") return false;
-
-  for (i=1; i<=9; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
-  Resto = (Soma * 10) % 11;
-
-    if ((Resto == 10) || (Resto == 11))  Resto = 0;
-    if (Resto != parseInt(strCPF.substring(9, 10)) ) return false;
-
-  Soma = 0;
-    for (i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i);
-    Resto = (Soma * 10) % 11;
-
-    if ((Resto == 10) || (Resto == 11))  Resto = 0;
-    if (Resto != parseInt(strCPF.substring(10, 11) ) ) return false;
-    return true;    
-}
-
-function Chamar(){
-    var strCPF = document.getElementById("cpf").value;
-    TestaCPF(strCPF);
-    if (TestaCPF(strCPF) == true) {
-
-    innerHTML = "Você digitou um CPF Válido!"
-    }else{
-    innerHTML = "Você digitou um CPF Inválido!"
+        if (localStorage.getItem("user") === null ){
+            dataBase.push(new User(user[0], user[1], user[2], user[3], user[4], user[5], user[7], user[8], user[9], user[10]));
+            localStorage.setItem("user", JSON.stringify(dataBase));
+            alert("Cadastro Realizado com Sucesso!");
+        }else{
+            dataBase = JSON.parse(localStorage.getItem("user"))
+            dataBase.push(new User(user[0], user[1], user[2], user[3], user[4], user[5], user[7], user[8], user[9], user[10]));
+            localStorage.setItem("user", JSON.stringify(dataBase));
+            alert("Cadastro Realizado com Sucesso!");
+        }
     } 
 }
 
@@ -97,30 +63,22 @@ function ValidPass(){
         }
 }
 
-function Validation(){
-dataBase = JSON.parse(localStorage.getItem("user"))
-let usern = document.getElementById("cpf").value;
-let pssw = document.getElementById("senha");
-    for (let i = 0; i < dataBase.length; i++){
-        alert(dataBase[i].documento)
-        alert(usern)
-        if(dataBase[i].documento === usern.value){
-            alert(qwe)
+function Validation(){ //validação de login e senha para entrar na area de perfil.
+    
+    for (i = 0; i <= dataBase.length; i++){
+        if(i == dataBase.length){
+            alert("Login Incorreto");
+        }else if (dataBase[i].cpf == usern.value || dataBase[i].email == usern.value){
             if(dataBase[i].pswd == pssw.value){
-                alert("Sucesso");
+                console.log("Sucesso");
                 localStorage.setItem('online', JSON.stringify(dataBase[i]));
-                // location.href="perfil.html"; // site à ser feita de perfil do usuario validado, ou window.open("home.html") para abrir em uma nova aba
+                location.href="perfil.html"; // site à ser feita de perfil do usuario validado, ou window.open("home.html") para abrir em uma nova aba
             }else{
                 alert("Senha Incorreta");
             }
             break;
         } 
     }
-}
-
-function mostraCadastro(){
-    $(".cadastro").show();
-    $(".login").hide();
 }
 
 function TestaCPF(strCPF) {
@@ -155,6 +113,12 @@ function Chamar(){
 }
 
 function Logout(){ // botão para sair do perfil validado para troca de perfil ou saida "segura" do sistema. 
+    dataBase = JSON.parse(localStorage.getItem("user"))
+    for(let i =0; i < dataBase.length; i++){
+        if (dataBase[i].documento === JSON.parse(localStorage.getItem("online")).documento){
+            dataBase[i] = JSON.parse(localStorage.getItem("online"))
+        }
+    }
     localStorage.removeItem("online");
     location.href="index.html"; //ou window.open("home.html") para abrir em uma nova aba
 }
@@ -177,6 +141,7 @@ function Add(){ //adicopnar certificados (array para os certificados)
         alert("Todos os campos devem ser preenchidos.");
     }              
 }
+
 function RemoveCert(){
     
    let confirm = prompt("Digite o nome do evento para confirmar:");
@@ -189,52 +154,51 @@ function RemoveCert(){
     };   
 }
 
-function Add(){
+let linhasTabela = getDataCertificate()
 
-    let certificado = Array.from(document.getElementsByName("addCerti")).map(function(element){
-        return element.value;});
-        let day1 = new Date(document.getElementById("day1").value.split('-').join('/')) 
-        let dataFormatada = ("0" + day1.getDate()).substr(-2) + "/" 
-        + ("0" + (day1.getMonth() + 1)).substr(-2) + "/" + day1.getFullYear();
-        let day2 = new Date(document.getElementById("day2").value.split('-').join('/'))
-        let dayFormat = ("0" + day2.getDate()).substr(-2) + "/" 
-        + ("0" + (day2.getMonth() + 1)).substr(-2) + "/" + day2.getFullYear(); //uso para formatar a data em pt-br
-
-    if (certificado[0] && certificado[1] && dayFormat && dataFormatada){
-    
-        if (localStorage.getItem("certificate") === null ){
-            dataCerti.push(new AddHour (certificado[0], dataFormatada, dayFormat, certificado[1], certificado[2]))
-                localStorage.setItem("certificate", JSON.stringify(dataCerti))
-                alert("Certificado adicionado com Sucesso!")
-        } else { 
-            
-            dataCerti = JSON.parse(localStorage.getItem("certificate"))  // for? //[teste 1]
-            dataCerti.push(new AddHour (certificado[0], dataFormatada, dayFormat, certificado[1], certificado[2]))
-            localStorage.setItem("certificate", JSON.stringify(dataCerti))
-            alert("Certificado adicionado com Sucesso!")
-        }
-    }else{  
-            alert("Todos os campos devem ser preenchidos.")
-        }  
-            
+function getDataCertificate() {
+    return JSON.parse(localStorage.getItem("certificate"));
 }
 
-// function RemoveCert(){
-//     localStorage.removeItem("certificate")
-//     let confirm = prompt("Digite o nome do evento para confirmar:")
+function criaTag(elemento) {
+    return document.createElement(elemento)
+}
+        
+let titulo = document.querySelector("h1"); 
+let tabela = document.getElementById("tabela");    
+let thead = criaTag("thead");
+let tbody = criaTag("tbody");
+let tfoot = criaTag("tfoot");    
+let indicesTabela = ["Evento", "Data Inicial", "Data Final", "Horas", "Tipo"];   
+let linhaHead = criaTag("tr");
+        
+function criaCelula(tag, text) {
+    tag = criaTag(tag);
+    tag.textContent = text;
+    return tag;
+}
     
-//     for (let i=0; i > dataCerti.length; i++){
-//         if(dataCerti[i].evento === confirm){
-//             localStorage.removeItem("certificate")
-//         }
-//     }
-// }
-// let linhaFoot = criaTag("tr");
-// let celulaFoot = criaCelula("td","Certistack");
-// celulaFoot.setAttribute("colspan",5);
-// linhaFoot.appendChild(celulaFoot);
-// tfoot.appendChild(linhaFoot);
+for(j = 0; j < indicesTabela.length; j++) {
+    let th = criaCelula("th", indicesTabela [j]);
+    linhaHead.appendChild(th);
+}
+thead.appendChild(linhaHead);
+
+for(j = 0; j < linhasTabela.length; j++) {
+    let linhaBody = criaTag("tr");
+
+    for(i = 0; i < linhasTabela[j].length; i++) {
+        cel = criaCelula("td", linhasTabela[j][i]);
+        linhaBody.appendChild(cel); 
+    }
+    tbody.appendChild(linhaBody);
+}
+let linhaFoot = criaTag("tr");
+let celulaFoot = criaCelula("td","Certistack");
+celulaFoot.setAttribute("colspan",5);
+linhaFoot.appendChild(celulaFoot);
+tfoot.appendChild(linhaFoot);
     
-// tabela.appendChild(thead);
-// tabela.appendChild(tbody);
-// tabela.appendChild(tfoot);
+tabela.appendChild(thead);
+tabela.appendChild(tbody);
+tabela.appendChild(tfoot);
